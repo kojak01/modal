fbb55258457d9670f4022f8732c49d10db6aa436
Konrad Piatek <konrad.jerzy.piatek@gmail.com>
2022-11-04 23:23:35 +0100

    add second button

--- js/script.js
+++ js/script.js
@@ -17,26 +17,37 @@ const modal = (body) => {
         <button class="modal-close-icon"><i data-feather="x"></i></button>
     </div>`;
 };
-
-
-const addToBasket = () => {
-  console.log('dodano do koszyka'); 
-  
+const createModal = (value) => {
   const modalContainer = document.createElement('div');
   modalContainer.className = 'modal-container';
-  modalContainer.innerHTML = modal({
+  modalContainer.innerHTML = modal(value);
+  document.body.appendChild(modalContainer);
+  feather.replace();
+};
+
+const addToBasket = () => {
+  console.log('dodano do koszyka');
+
+  createModal({
     title: 'lorem ipsum dolor title vla blala laslal llas',
     description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui voluptatibus error, ullam ab quos iusto minus! Natus, optio quasi magni fugiat neque atque error unde.',  
     accept_btn: 'Zaakceptuj',
     decline_btn: 'Anuluj',
 });
-  document.body.appendChild(modalContainer);
-  feather.replace(); //<-icon "i"
 };
 
 actionBtn.addEventListener('click', addToBasket);
 
-
+const actionTwoBtn = document.querySelector('.action-two-btn');
+actionTwoBtn.addEventListener('click', () => {
+  createModal({
+    title: 'Drugi tekst modala',
+    description: 'lapapa ldsald kdlk saldksaldksadlsak llsakds. Qui voluptatibus error, ullam ab quos iusto minus! Natus, optio quasi magni fugiat neque atque error unde.',  
+    accept_btn: 'OK',
+    decline_btn: 'Nie', 
+  });
+} );
+ 
 
 
 
